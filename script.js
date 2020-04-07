2b9affa8701f71d0a5b45314e1e590e72ec603e2
Scott Ratigan <scotteratigan@gmail.com>
2020-04-07 09:01:11 -0700

    added refs to track changed nodes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let myCount = {
-  val: 1
-};
+'use strict'
 
+let myNum = 1
 
 /*
   Make root element object, contains meta-info
@@ -46,7 +45,8 @@ const virtualDOM = [
               },
               {
                 elm: 'span',
-                children: () => myCount.val
+                classes: 'ml',
+                children: () => myNum
               }
             ]
           }
@@ -54,59 +54,78 @@ const virtualDOM = [
       }
     ]
   }
-];
+]
 
 function increaseCount() {
-  console.log('increasing count...')
-  myCount.val += 1;
-  window.myCount = myCount;
-  loadNodes();
+  myNum += 1
+  loadNodes()
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  loadNodes();
-});
+document.addEventListener('DOMContentLoaded', loadNodes)
 
 function loadNodes() {
-  const root = document.getElementById('root');
-  console.log('root', root)
-  const newRoot = document.createElement('div');
-  newRoot.id = 'root';
-  const newNodes = recursiveCreate(virtualDOM);
-  newNodes.forEach(node => newRoot.appendChild(node));
-  console.log('Loaded nodes:');
-  console.log(virtualDOM);
-  root.replaceWith(newRoot);
+  const root = document.getElementById('root')
+  const newRoot = document.createElement('div')
+  newRoot.id = 'root'
+  const newNodes = recursiveCreate(virtualDOM)
+  newNodes.forEach(node => newRoot.appendChild(node))
+  root.replaceWith(newRoot)
   // Create global for reference elsewhere:
-  // window.vDOM = virtualDOM;
+  window.vDOM = virtualDOM
+  console.log(window.vDOM)
 }
 
-
-function recursiveCreate(nodeList) {
-  // nodeList is an array of objects that can be used to create nodes
-  return nodeList.map(node => {
-    console.log('creating node:', node)
-    const { elm, id, classes, onClick, children } = node;
-    const newNode = document.createElement(elm);
-    node.ref = newNode;
-    if (id) newNode.id = id;
+function recursiveCreate(vDom) {
+  // vDom is an array of objects that can be used to create html nodes
+  return vDom.map(node => {
+    const { elm, id, classes, onClick, children } = node
+    const newNode = document.createElement(elm)
+    let content = null // used because we don't want to recursively get textContent
+    node.ref = newNode
+    if (id) newNode.id = id
     if (classes) {
       classes.split(' ').forEach(newClass => newNode.classList.add(newClass))
     }
-    if (onClick) newNode.addEventListener("click", onClick);
+    if (onClick) newNode.addEventListener('click', onClick)
     if (children) {
-      if (typeof children === "string" || typeof children === "number") {
-        // todo: handle case of function
-        newNode.textContent = children;
-      } else if (typeof children === "function") {
-        newNode.textContent = children();
-      }else if (Array.isArray(children)) {
-        const nodeChildren = recursiveCreate(children);
-        nodeChildren.forEach(child => newNode.appendChild(child));
+      if (typeof children === 'string' || typeof children === 'number') {
+        newNode.textContent = children
+        content = newNode.textContent
+      } else if (typeof children === 'function') {
+        newNode.textContent = children()
+        content = newNode.textContent
+      } else if (Array.isArray(children)) {
+        const nodeChildren = recursiveCreate(children)
+        nodeChildren.forEach(child => newNode.appendChild(child))
       } else {
-        throw new Error("Unknown typeof value for child.");
+        throw new Error('Unknown typeof value for child.')
       }
     }
-    return newNode;
+    node.hash = hashNode(node, content)
+    if (content) console.log('hash:', node.hash)
+    return newNode
   })
 }
+
+function hashNode(node, textContent) {
+  let str = textContent || ''
+  Object.entries(node).map(([entry, value]) => {
+    if (entry !== 'children' && entry !== 'ref' && entry !== 'hash') {
+      str += value
+    }
+  })
+  return hashStr(str)
+}
+
+function hashStr(str) {
+  const len = str.length
+  let hash = 0
+  let chr = null
+  for (let i = 0; i < len; i++) {
+    chr = str.charCodeAt(i)
+    hash = ((hash << 5) - hash) + chr
+    hash |= 0 // Convert to 32bit integer
+  }
+  return hash
+}
+
